e89615c9ffc6a4c1f6103819023a41c7c52eee6a
Paul Trippett <paul@trippett.org>
2015-03-02 09:55:12 +0000

    Handle oauth token requests in-gem

--- lib/sendvia/session.rb
+++ lib/sendvia/session.rb
@@ -1,11 +1,36 @@
+require 'oauth2'
+
 module Sendvia
 
   class Session
+    SITE = 'https://www.sendvia.co.uk'
+    TOKEN_URL = '/alpha5/token'
+    AUTH_URL = '/alpha5/account/authorize'
+
+    attr_accessor :client_id
+    attr_accessor :client_secret
     attr_accessor :access_token
 
-    def initialize(access_token, activate_session = false)
-      self.access_token = access_token
-      Sendvia::Base.activate_session(self) if activate_session
+    def initialize(client_id, client_secret, activate_session = false)
+      self.client_id = client_id
+      self.client_secret = client_secret
+      setup_session if activate_session
+    end
+
+    private
+
+    def setup_session
+      self.access_token = get_token
+      Sendvia::Base.activate_session(self)
+    end
+
+    def get_token
+      client = OAuth2::Client.new(self.client_id,
+                                  self.client_secret,
+                                  site: SITE,
+                                  token_url: TOKEN_URL,
+                                  authorize_url: AUTH_URL)
+      client.client_credentials.get_token.token
     end
   end
 end
